9285567c7318eb15d83f0d38d2c23b476e28d479
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-21 15:50:52 -0800

    pass triangle constructor test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -6,6 +6,12 @@
   [TestClass]
   public class TriangleTests
   {
-    
+
+    [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    {
+      Triangle newTriangle = new Triangle();
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
   }
  }

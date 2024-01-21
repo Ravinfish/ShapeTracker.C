9414e60d35516a269691abf0f186556334c1d46f
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-21 12:55:38 -0800

    added class with triangle types

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -7,8 +7,13 @@ namespace ShapeTracker
   {
     static void Main()
     {
-      Triangle testTriangle = new Triangle();
-      Console.WriteLine(testTriangle.GetType());
+      Triangle testTriangle = new Triangle(10,10,10);
+      Console.WriteLine($"Side one of the triangle: {testTriangle.Side1}");
+      Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
+      Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
+      Console.WriteLine("Want to know what type of triangle you have?");
+      Console.WriteLine("Calculating...");
+      Console.WriteLine($"Yourtriangle is: {testTriangle.CheckType()}");
     }
   }
 }

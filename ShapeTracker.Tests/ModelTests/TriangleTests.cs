7cf9d00b10d81d7a9777ddfe0583785a2c9bf33c
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-21 15:34:43 -0800

    mstest setup

---
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,11 @@
+ using Microsoft.VisualStudio.TestTools.UnitTesting;
+ using ShapeTracker.Models;
+
+ namespace ShapeTracker.TestTools
+ {
+  [TestClass]
+  public class TriangleTests
+  {
+    
+  }
+ }

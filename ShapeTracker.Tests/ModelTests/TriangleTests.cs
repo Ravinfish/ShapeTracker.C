b3bdde9d28b76dfa9283d65b3b17e1e4d9dc5b69
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-21 16:12:56 -0800

    pass test to add side1

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -1,8 +1,8 @@
- using Microsoft.VisualStudio.TestTools.UnitTesting;
- using ShapeTracker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
 
- namespace ShapeTracker.TestTools
- {
+namespace ShapeTracker.Tests
+{
   [TestClass]
   public class TriangleTests
   {
@@ -10,8 +10,18 @@
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle();
+      Triangle newTriangle = new Triangle(2);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      int length1 = 3;
+      Triangle newTriangle = new Triangle(length1);
+      int result = newTriangle.Side1;
+      Assert.AreEqual(length1, result);
+    }
+
   }
- }
+}
